fa8071bf6f31a038a116926d7fe8403d6171a066
KuroMimeow <sirinda144244@gmail.com>
2026-02-02 23:56:15 +0700

    my-profile-card finish

---
+++ lab4/my-profile-card/src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import profileCard from './components/ProfileCard.vue';
+</script>
+
+<template>
+  <div class="grid grid-cols-3 place-items-center 
+    min-h-screen bg-gradient-to-br from-pink-200 to-yellow-100">
+    
+    <profileCard 
+    background="./src/asset/background.jpg"
+    profileimg="./src/asset/image.jpg"
+    name="Sirinda Charoenwai"
+    subheading="Florist, 20, BKK"
+    followers="10M"
+    following="11"
+    projects="1"
+    instargram="https://www.instagram.com/yyy__iiiev?igsh=NG9jbzdxZmQ4eGlk&utm_source=qr"
+    tiktok="https://www.tiktok.com/@kuromim3ow_?_r=1&_t=ZS-93ae40s6wKb"
+    facebook="https://www.facebook.com/eve.sirinda.3"
+    />
+
+    <profileCard
+      background="./src/asset/background.jpg"
+      profileimg="./src/asset/pond.jpg"
+      name="Sakdinan Tankammun"
+      subheading="Reseller sneaker, 20, BKK"
+      followers="70,000"
+      following="1,223"
+      projects="12"
+      instargram="https://www.instagram.com/pangp.0nd?igsh=MWZmcTBpcHQ1YjNnbg==" 
+      tiktok="https://www.tiktok.com/@cow_pond?_r=1&_t=ZS-93ae5ELyvTw"
+      facebook="https://www.facebook.com/profile.php?id=100016575914378"
+    />
+
+    <profileCard 
+      background="./src/asset/background.jpg"
+      profileimg="./src/asset/tanny.jpg"
+      name="Siriyakorn Saengkaew"
+      subheading="UX/UI designer, 20, BKK"
+      followers="5000M"
+      following="0"
+      projects="1M"
+      instargram="https://www.instagram.com/siri_yx?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+      tiktok="https://www.tiktok.com/@tachii67?_r=1&_t=ZS-93ae5uVPOkf"
+      facebook="https://www.facebook.com/tan.siriyagon"
+    />
+ </div>
+  
+
+</template>
+
+<style scoped></style>
---
+++ lab4/my-profile-card/src/components/ProfileCard.vue
@@ -0,0 +1,89 @@
+<script setup>
+defineProps([
+    "profileimg",
+    "background",
+    "name",
+    "subheading",
+    "followers",
+    "following",
+    "projects",
+    "instargram",
+    "tiktok",
+    "facebook"
+])
+</script>
+
+<template>
+    <!-- card -->
+    <div class="bg-white shadow-lg rounded-2xl min-[320px] overflow-hidden text-center">
+        <!-- image -->
+        <div class="relative min-h-32 bg-cover bg-center" 
+            :style="'background-image: url(' + background + ')'">
+            <img :src="profileimg" 
+                alt="Profile Image" 
+                class="inline-block w-32 h-32 rounded-full object-center object-cover border-6 border-white 
+                absolute top-full left-1/2 transform -translate-1/2"
+            >
+        </div>
+
+        <!-- subheading -->
+        <div class="px-6 pt-20 pb-6">
+            <h2 class="font-bold text-gray-800 text-2xl mb-2">
+                {{ name }}
+            </h2>
+
+            <h3 class="text-gray-600 text-lg">
+                {{ subheading }}
+            </h3>
+        </div>
+        <!-- social -->
+        <div class="flex justify-center gap-4">
+            <a v-if="instargram" :href="instargram" target="_blank" class="text-pink-200 hover:text-pink-400 font-medium">
+                Instagram
+            </a>
+            <a v-if="tiktok" :href="tiktok" target="_blank" class="text-gray-200 hover:text-gray-600 font-medium">
+                Tiktok
+            </a>
+            <a v-if="facebook" :href="facebook" target="_blank" class="text-blue-200 hover:text-blue-400 font-medium">
+                Facebook
+            </a>
+
+        </div>
+
+        <!-- stats -->
+        <div class="p-6 border-t border-gray-200 flex item-center justify-center gap-6">
+            <!-- follower -->
+            <div>
+                <h4 class="text-gray-600 text-sm">
+                    Followers
+                </h4>
+                <p class="font-bold text-gray-600 text-2xl">
+                    {{ followers }}
+                </p>
+            </div>
+            <!-- following -->
+            <div>
+                <h4 class="text-gray-600 text-sm">
+                    Following
+                </h4>
+                <p class="font-bold text-gray-600 text-2xl">
+                    {{ following }}
+                </p>
+            </div>
+            <!-- project -->
+            <div>
+                <h4 class="text-gray-600 text-sm">
+                    Projects
+                </h4>
+                <p class="font-bold text-gray-600 text-2xl">
+                    {{ projects }}
+                </p>
+            </div>
+        </div>
+
+        
+    </div>
+     
+</template>
+
+<style scoped></style>
